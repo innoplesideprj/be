7fd6222ef40a31860c740b5ccd07dd9e5155f168
bestinwoo <bestinwoo@gmail.com>
2023-11-20 00:01:15 +0900

    first commit

---
+++ src/main/java/sideproject/meet/MeetApplication.java
@@ -0,0 +1,13 @@
+package sideproject.meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetApplication.class, args);
+	}
+
+}
